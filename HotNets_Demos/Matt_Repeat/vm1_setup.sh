d5f64dcb33b144e324d86338bee9372dcc23de15
slab14 <30778419+slab14@users.noreply.github.com>
2018-01-03 11:39:36 -0500

    typo fix

--- HotNets_Demos/Matt_Repeat/vm1_setup.sh
+++ HotNets_Demos/Matt_Repeat/vm1_setup.sh
@@ -5,7 +5,7 @@ echo "IP=\$1
 curl --connect-timeout 5 -s -X PUT http://\$IP:8000/api/newdeveloper/groups/0/action -d {\\\"on\\\":false}" > attack.sh
 
 touch legitimate.sh
-echo "IP=$\1
+echo "IP=\$1
 curl -U tommy:iotsec --connect-timeout 5 -s -X PUT http://\$IP:8000/api/newdeveloper/groups/0/action -d {\\\"on\\\":true}" > legitimate.sh
 
 chmod +x attack.sh legitimate.sh
